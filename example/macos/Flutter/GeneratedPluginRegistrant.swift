a1f294fb14886056d9395e3da16ffd39cf73a399
zonble <zonble@gmail.com>
2021-11-26 10:20:47 +0800

    Implements the plug-in on macOS.

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_window_close
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterWindowClosePlugin.register(with: registry.registrar(forPlugin: "FlutterWindowClosePlugin"))
+}
