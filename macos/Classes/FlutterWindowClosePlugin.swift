6fe6ff09bb69c4c7199e403077c220138f070528
zonble <zonble@gmail.com>
2024-09-08 20:19:50 +0800

    Modifies the way to choose the active window on macOS.

--- macos/Classes/FlutterWindowClosePlugin.swift
+++ macos/Classes/FlutterWindowClosePlugin.swift
@@ -2,15 +2,20 @@ import Cocoa
 import FlutterMacOS
 
 public class FlutterWindowClosePlugin: NSObject, FlutterPlugin, NSWindowDelegate {
+    /// The window that contains the view that is registering the plugins.
     private var window: NSWindow?
+    /// The method channel to notify the Flutter side.
     private var notificationChannel: FlutterMethodChannel?
+    /// If the plugin is initialized.
     private var initialized = false
 
     public static func register(with registrar: FlutterPluginRegistrar) {
-        let channel = FlutterMethodChannel(name: "flutter_window_close", binaryMessenger: registrar.messenger)
+        let channel = FlutterMethodChannel(
+            name: "flutter_window_close", binaryMessenger: registrar.messenger)
         let instance = FlutterWindowClosePlugin()
-        instance.notificationChannel = FlutterMethodChannel(name: "flutter_window_close_notification", binaryMessenger: registrar.messenger)
-        instance.window = NSApp.windows.first
+        instance.notificationChannel = FlutterMethodChannel(
+            name: "flutter_window_close_notification", binaryMessenger: registrar.messenger)
+        instance.window = registrar.view?.window
         instance.window?.delegate = instance
         registrar.addMethodCallDelegate(instance, channel: channel)
     }
@@ -30,6 +35,8 @@ public class FlutterWindowClosePlugin: NSObject, FlutterPlugin, NSWindowDelegate
             result(FlutterMethodNotImplemented)
         }
     }
+
+    //MARK: - NSWindow delegate
 
     public func windowShouldClose(_ sender: NSWindow) -> Bool {
         if initialized == false {
