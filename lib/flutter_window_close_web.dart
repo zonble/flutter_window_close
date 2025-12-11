zonble/flutter_window_close

commit 166df017c8d050e5dcb58a92e189ce177dde6477

fix after review

lib/flutter_window_close_web.dart
@@ -27,8 +27,10 @@ class FlutterWindowClosePluginWeb {
       'beforeunload',
       (JSAny event) {
         if (event.isA<BeforeUnloadEvent>()) {
+          final unloadEvent = event as BeforeUnloadEvent;
+          unloadEvent.preventDefault();
           if (_returnValue != null) {
-            (event as BeforeUnloadEvent).returnValue = _returnValue!;
+            unloadEvent.returnValue = _returnValue!;
           }
         }
       }.toJS,

commit 4ea282feb24469023fa59adbaa4d416c78e53ef8

Web: Call preventDefault only when the plugin was enabled explicitly by setWebReturnValue
It was not possible to disable the behavior at the moment. Browsers understanding preventDefault() would always show the dialog as long as the plugin is enabled.

Broken in commit 166df017c8d050e5dcb58a92e189ce177dde6477.

lib/flutter_window_close_web.dart
@@ -28,8 +28,8 @@ class FlutterWindowClosePluginWeb {
       (JSAny event) {
         if (event.isA<BeforeUnloadEvent>()) {
           final unloadEvent = event as BeforeUnloadEvent;
-          unloadEvent.preventDefault();
           if (_returnValue != null) {
+            unloadEvent.preventDefault();
             unloadEvent.returnValue = _returnValue!;
           }
         }